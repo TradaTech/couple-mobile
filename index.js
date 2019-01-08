45d90bb7d900d9f5ae52c2cfd92c19f3ba7cd05c
tungDuong <tungduonghgg123@gmail.com>
2019-01-08 11:25:35 +0700

    Complete header text

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
 import React  from 'react'
 import { AppRegistry, Text, View, YellowBox } from 'react-native'
+import Header from './src/Header'
 
 const App = () => {
-    const { textStyle, viewStyle } = styles
-    console.log("ahihi")
+    
     return (
-        <View style={viewStyle}>
-            <Text >Hello World</Text>
+        <View>
+            <Header headerText={'Add Memory'} />
         </View>
     )
     
 }
-const styles = {
-    viewStyle: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 100,
-        paddingTop: 15,
-        backgroundColor: '#F8F8F8',
-        fontSise: 40
-    },
-    textStyle: {
-        fontSise: 20
-    }
-}
+
 YellowBox.ignoreWarnings(['Remote debugger']);
 AppRegistry.registerComponent('coupleMobile', () => App)
---
+++ src/Header.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { View, Text} from 'react-native'
+
+const Header = ( props ) => {
+    const { textStyle, viewStyle,containerStyle } = styles
+    return (
+        <View style={containerStyle}>
+            <View style={viewStyle}>
+                <Text style={textStyle}>{props.headerText}</Text>
+            </View>
+            
+        </View>
+    )
+}
+const styles = {
+    viewStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 30,
+        
+
+    },
+    containerStyle: {
+        height: 72,
+        backgroundColor: '#b276ff',
+    },
+    textStyle: {
+        fontSise: 16,
+        color: '#ffffff',
+        // fontWeight: 600,
+        // fontFamily: 'Montserrat'
+    }
+}
+export default Header
